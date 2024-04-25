55cad32a5cd7b68ee0611dd9bad98c37411b2fa0
Joel Lehtonen <jlehtone@c2r5p9.hive.fi>
2024-04-25 16:15:30 +0300

    putchar, putendl, and putstr done

---
+++ ft_putendl_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlehtone <jlehtone@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 15:11:32 by jlehtone          #+#    #+#             */
+/*   Updated: 2024/04/25 15:18:54 by jlehtone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	write(fd, s, len);
+	write(fd, "\n", 1);
+}
+
+int	main(void)
+{
+	ft_putendl_fd("this is followed by a newline \n", 1);
+}
